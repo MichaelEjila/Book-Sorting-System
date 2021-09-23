3077d6ed248982380c20fa9b6bd1228a0acf8267
Michael Ejila <michaelejila13gmail.com>
2021-09-23 12:40:36 -0400

    Input removed, Sample added

--- Book Sorting System.py
+++ Book Sorting System.py
@@ -1,26 +1,19 @@
 # list to hold all books and books sorted
-book_list = []
+book_list = [["Pride and Prejudice", "Jane austen", "0061964360"],
+             ["The Instrumentality of Mankind", "Cordwainer Smith", "0345277163"],
+             ["Children of the Lens", "E.E Doc Smith", "9780586038475"]]
 sorted_book_list = []
 
 
 # Function to sort newly added book
 def add_book():
-    book_list.append([book_name, book_author, book_isbn])
     # Moving all book entries into new list sorted
     sorted_book_list = (sorted(book_list))
-    print("This list of books in alphabetical order are\n", sorted_book_list)
+    print("This list of books in alphabetical order are:", sorted_book_list, sep='\n')
     return sorted_book_list
 
 
 # Main to request input of book name, author and ISBN number
-a = "a"
-# Loop to keep adding books till user is done
-while a.upper() == "A":
-    book_name = input("Enter book name: ")
-    book_author = input("Enter book Author: ")
-    book_isbn = int(input("Enter book International Standard Number: "))
-    add_book()
-    a = input("Enter A to add another book or any other letter to end ")
-    # condition to leave loop
-    if a.upper() != "A":
-        break
+print("The original list of books are: ", *book_list, sep='\n')
+a = input("Press enter to sort alphabetically by title ")
+add_book()
